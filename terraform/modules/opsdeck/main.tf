29b017c3515686a6518c5c93ba8ff1b25f044c04
Leon Spors <15744333+LeonSpors@users.noreply.github.com>
2026-07-20 20:54:49 +0200

    Format RBAC Helm values

--- terraform/modules/opsdeck/main.tf
+++ terraform/modules/opsdeck/main.tf
@@ -79,7 +79,9 @@ resource "helm_release" "vertex" {
       tls  = false
     }
     postgres = { existingSecret = kubernetes_secret.postgres.metadata[0].name }
-    rbac = { managedNamespaces = [var.namespace] }
+    rbac = {
+      managedNamespaces = [var.namespace]
+    }
   })]
   depends_on = [helm_release.postgres, helm_release.redis]
 }
